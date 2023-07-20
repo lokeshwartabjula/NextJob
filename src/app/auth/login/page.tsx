7d5be893db6c3f7d56297948693481471e099fb2
Aayush Dakwala <ay383119@dal.ca>
2023-07-20 18:09:30 -0300

    Added Login Signup pages

---
+++ src/app/auth/login/page.tsx
@@ -0,0 +1,137 @@
+"use client";
+
+import Head from "next/head";
+import NextLink from "next/link";
+import { useRouter } from "next/navigation";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+import { Box, Button, Link, Stack, TextField, Typography } from "@mui/material";
+import AuthLayout from "../layout";
+import React, { ReactElement } from "react";
+
+function Page(): ReactElement {
+  const router = useRouter();
+  const [isHydrated, setIsHydrated] = React.useState(false);
+
+  React.useEffect(() => {
+    setIsHydrated(true);
+  }, []);
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+      submit: null,
+    },
+    validationSchema: Yup.object({
+      email: Yup.string()
+        .email("Must be a valid email")
+        .max(255)
+        .required("Email is required"),
+      password: Yup.string().max(255).required("Password is required"),
+    }),
+    onSubmit: async (values, helpers) => {
+      try {
+        // Login backend call
+        router.push("/");
+      } catch (err: any) {
+        helpers.setStatus({ success: false });
+        helpers.setErrors({ submit: err.message });
+        helpers.setSubmitting(false);
+      }
+    },
+  });
+
+  return (
+    <>
+      <Head>
+        <title>Login</title>
+      </Head>
+      <Box
+        sx={{
+          backgroundColor: "background.paper",
+          flex: "1 1 auto",
+          alignItems: "center",
+          display: "flex",
+          justifyContent: "center",
+        }}
+      >
+        <Box
+          sx={{
+            maxWidth: 550,
+            px: 3,
+            py: "100px",
+            width: "100%",
+          }}
+        >
+          <div>
+            <Stack spacing={1} sx={{ mb: 3 }}>
+              <Typography variant="h4">Login</Typography>
+            </Stack>
+            <form noValidate onSubmit={formik.handleSubmit}>
+              {isHydrated && (
+                <Stack spacing={3}>
+                  <TextField
+                    error={!!(formik.touched.email && formik.errors.email)}
+                    fullWidth
+                    helperText={formik.touched.email && formik.errors.email}
+                    label="Email Address"
+                    name="email"
+                    onBlur={formik.handleBlur}
+                    onChange={formik.handleChange}
+                    type="email"
+                    value={formik.values.email}
+                  />
+                  <TextField
+                    error={
+                      !!(formik.touched.password && formik.errors.password)
+                    }
+                    fullWidth
+                    helperText={
+                      formik.touched.password && formik.errors.password
+                    }
+                    label="Password"
+                    name="password"
+                    onBlur={formik.handleBlur}
+                    onChange={formik.handleChange}
+                    type="password"
+                    value={formik.values.password}
+                  />
+                </Stack>
+              )}
+
+              {formik.errors.submit && (
+                <Typography color="error" sx={{ mt: 3 }} variant="body2">
+                  {formik.errors.submit}
+                </Typography>
+              )}
+              <Button
+                fullWidth
+                size="large"
+                sx={{ mt: 3 }}
+                type="submit"
+                variant="contained"
+              >
+                Login
+              </Button>
+              <Typography color="text.secondary" variant="body2">
+                Don&apos;t have an account? &nbsp;
+                <Link
+                  component={NextLink}
+                  href="/auth/register"
+                  underline="hover"
+                  variant="subtitle2"
+                >
+                  Register
+                </Link>
+              </Typography>
+            </form>
+          </div>
+        </Box>
+      </Box>
+    </>
+  );
+}
+
+Page.getLayout = (page: ReactElement) => <AuthLayout>{page}</AuthLayout>;
+
+export default Page;
