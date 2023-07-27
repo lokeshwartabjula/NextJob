da1b7cb2be00ead1daba101753452bd3c90e8ec9
kbpanchal <kr946702@dal.ca>
2023-07-26 22:45:30 -0300

    Added comments for the files

--- frontend/src/app/home/RecrutingOption.tsx
+++ frontend/src/app/home/RecrutingOption.tsx
@@ -1,3 +1,9 @@
+/*
+  Author: Kruti Panchal
+  Banner Id: B00930563
+  email id: kr946702@dal.ca
+*/
+
 "use client";
 import {Box, Grid, Button, useMediaQuery, useTheme, Typography} from '@mui/material';
 
--- frontend/src/app/home/types.ts
+++ frontend/src/app/home/types.ts
@@ -1,3 +1,9 @@
+/*
+  Author: Kruti Panchal
+  Banner Id: B00930563
+  email id: kr946702@dal.ca
+*/
+
 import { IconDefinition } from "@fortawesome/fontawesome-common-types";
 
 export type JobStepsInformationType = {
