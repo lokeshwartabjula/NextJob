093c41a4a9d20a8e06f41f0b43fa020a4cdd32ac
Aayush Dakwala <ay383119@dal.ca>
2023-08-05 22:35:04 -0300

    Updated dashboard

--- frontend/src/app/dashboard/types.ts
+++ frontend/src/app/dashboard/types.ts
@@ -20,11 +20,41 @@ export interface ICity {
 }
 
 export interface ApexOptionsModified extends ApexOptions {
-    chart: ApexChartCustom;
-  }
+  chart: ApexChartCustom;
+}
 
-interface ApexChartCustom extends ApexChart{
+interface ApexChartCustom extends ApexChart {
   type: "line" | "bar" | "donut" | "pie" | undefined;
+}
+
+export interface RawJobType {
+  id: string;
+  jobTitle: string;
+  jobDescription: string;
+  skills: string[];
+  jobStatus: string;
+  noOfPositions: string;
+  jobType: string;
+  location: {
+    type: string;
+    coordinates: number[];
+    placeId: string;
+    placeName: string;
+    city: string;
+    state: string;
+    country: string;
+  };
+  salary: string;
+  experience: string;
+  openDate: string;
+  employerId: string;
+  jobCompany: string;
+  jobCompanyLogo: string;
+}
+
+export interface ChartMetaDataType {
+  label: string;
+  data: number;
 }
 
 export type GraphType = "line" | "bar" | "donut" | "pie";
